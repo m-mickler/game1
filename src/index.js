8fcb182761a358fa23f28d59b5f630f96ca436ab
Alexander Cobble <acobble@Alexanders-MacBook-Pro.local>
2022-08-16 17:13:45 -0700

    Created Ship class and renders Hello World

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+
+class Ship extends React.Component{
+    constructor(props){
+        super(props)
+     
+    }
+    render(){
+        return(
+            <div> Hello World</div>
+        );
+    }
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Ship />);
