e85a54278dce806c3f693e7d056f7f2c7e0be4e7
Matthew Mickler <matthewmickler@Matthews-MBP.hitronhub.home>
2022-08-16 21:23:26 -0700

    ship shape to triangle, WASD movement keys finished

--- src/index.js
+++ src/index.js
@@ -13,5 +13,28 @@ class Ship extends React.Component{
     }
 }
 
+document.onkeydown = detectKey;
+function detectKey(e) {
+    var posLeft = document.querySelector('.ship').offsetLeft;
+    var posTop = document.querySelector('.ship').offsetTop;
+    e = e || window.event;
+    if (e.keyCode == '87') {
+        // up arrow
+        document.querySelector('.ship').style.top  = (posTop-25)+"px";
+    }
+    else if (e.keyCode == '83') {
+        // down arrow
+        document.querySelector('.ship').style.top  = (posTop+25)+"px";
+    }
+    else if (e.keyCode == '65') {
+       // left arrow
+        document.querySelector('.ship').style.left  = (posLeft-25)+"px";
+    }
+    else if (e.keyCode == '68') {
+       // right arrow
+        document.querySelector('.ship').style.left  = (posLeft+25)+"px";
+    }
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Ship />);
